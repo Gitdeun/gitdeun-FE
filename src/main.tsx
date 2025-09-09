51a595cba8e8c9fbda95c7cfb4f32347a33b932d
oh-chaeyeon <chaebread555@gmail.com>
2025-09-10 08:31:59 +0900

    chore : @mantine/notifications 설치

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { MantineProvider } from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
 import App from './App.tsx';
 import './index.css';
 
@@ -12,6 +13,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <MantineProvider defaultColorScheme="light" theme={{}}>
+        <Notifications position="top-center" />
         <App />
       </MantineProvider>
     </BrowserRouter>
